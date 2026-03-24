ed876a287ac9569f44ab0562b7c53107763cee95
João J <etiotndn@gmail.com>
2026-03-23 22:04:16 -0300

    feat: adiciona sistema de deploy pelo github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/reactflow-funil-de-trafego',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
